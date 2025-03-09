81eead7f263e8276c22903f1ca0333716bd8ef06
Akshay_M <harshaakshay14@gmail.com>
2025-03-09 22:12:11 +0530

    added .env file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgZh6t_miuSmm7AWI3ZviujZe-9ZuSKY4",
-  authDomain: "task-manager-26b1d.firebaseapp.com",
-  projectId: "task-manager-26b1d",
-  storageBucket: "task-manager-26b1d.firebasestorage.app",
-  messagingSenderId: "490733967589",
-  appId: "1:490733967589:web:1336f2d5a679eaf8d15c0c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
